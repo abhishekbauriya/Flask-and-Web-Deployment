a8f78dc922da8a95938f5d3e4fe931c0b9b50d7b
abhishekbauriya <abhishekbauriya007@gmail.com>
2021-02-23 19:34:24 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect, request
+
+# __name__ == __main__
+app = Flask(__name__)
+
+friends = ["Prateek", "Jatin", "Ashish"]
+
+
+@app.route('/')
+def hello():
+	return render_template("index.html", my_friends = friends)
+
+@app.route('/about')
+def about():
+	return "<h1> About Page </h1>"
+
+@app.route('/home')
+def home():
+	return redirect('/')
+
+@app.route('/submit', methods = ['POST'])
+def submit_data():
+	if request.method == 'POST':
+		no1 = int(request.form['no1'])
+		no2 = int(request.form['no2'])
+
+		f = request.files['userfile']
+		# To save the file
+		f.save(f.filename)
+
+	return str(no1+no2)
+
+
+
+if __name__ == '__main__':
+	#app.debug = True
+	app.run(debug = True)
